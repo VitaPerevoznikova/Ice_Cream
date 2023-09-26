42081910188f99a3a0fad875fbfca092443bd7cb
VitaPerevoznikova <129975564+VitaPerevoznikova@users.noreply.github.com>
2023-09-26 14:55:41 +0000

    Deploying to gh-pages from @ VitaPerevoznikova/Ice_Cream@480058709bf1ed93e8478a0b478fc1433ad1a858 🚀

---
+++ index.7e95766f.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".svg-open-menu"),closeMenuBtn:document.querySelector(".menu-close"),menu:document.querySelector(".mobile-menu"),menuList:document.querySelector(".mobile-menu__list")};function n(){document.body.classList.toggle("no-scroll"),e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n),e.menuList.addEventListener("click",(function(){e.menu.classList.add("is-hidden"),document.body.classList.remove("no-scroll")}))}();
+//# sourceMappingURL=index.7e95766f.js.map
